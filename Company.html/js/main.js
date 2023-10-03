9f3fc19fafe4011c9db5d2902a76c6da5f6d5b05
SUBHASHNEE THAKUR <126400709+Subhashnee15@users.noreply.github.com>
2023-10-04 01:15:47 +0530

    Add files via upload

---
+++ Company.html/js/main.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+
+    $('.fa-bars').click(function(){
+        $(this).toggleClass('fa-items');
+        $('.navbar').toggleclass
+    });
+    $(window).on('load scroll',function(){
+        $('.fa-bars').removeClass('fa-items');
+        $('.navbar').removeClass('nav-toggle');
+        if($(window).scrollTop()>35)
+        {
+            $('.header').css({'background':'#002e5f','box-shadow':'0 .2rem  .5rem rgba(0,0,0,.4)'});
+
+        }
+        else{
+            $('.header').css({'background':'none','box-shadow':'none'});
+        }
+    });
+const counters = document.querySelectorAll('.counter');
+const speed = 120;
+counter.foreach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count =  +counter.innerText;
+        const inc =  target/speed;
+        if (count < target) {
+            counter.innerText = count + inc;
+            setTimeout(updateCount ,1);
+        }
+        else{
+            counter.innerText = target;
+        }
+    };
+    updateCount();
+});
+(function($){
+    "use strict";
+    $(".clients-carousel").owlCarousel(
+        {
+            autoplay : true,
+            dots: true,
+            loop: true,
+            responsive: {0: {items: 2}, 768: {items: 4}, 900 : {items: 6} }
+
+        });
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {0: {items:1}, 576: {items: 2}, 768: {items: 3}, 992: {items: 4}}
+    });
+})(jQuery);
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 100){
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
+$('.back-to-top').click(function() {
+    $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    return false;
+});
+$('.accordion-header').click(function(){
+    $('.accordion .accordion-body').slideup(500);
+    $(this).next('.accordion-body').slideDown(500);
+    $('.accordion .accordion-header span').text('+');
+    $(this).childern('span').text('-');
+});
+});
